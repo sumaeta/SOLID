762e144b4eed617b65d1a8f58b63c4c548dc54ad
sumaeta <brenosumaeta33@gmail.com>
2021-07-20 11:00:25 -0300

    Finalizando o Principio da Responsabilidade Unica

--- src/SRP/problem/OrdemDeCompra.java
+++ src/SRP/problem/OrdemDeCompra.java
@@ -1,4 +1,43 @@
 package SRP.problem;
 
+import SRP.solution.Produto;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrdemDeCompra {
+
+    private List<SRP.solution.Produto> produtos = new ArrayList<>();
+
+    public void adicionarProduto( ){
+        //código para adicionar um produto
+    }
+
+    public void removerProduto( ){
+        //código para remover um produto
+    }
+
+    public BigDecimal calcularTotal(){
+        return produtos.stream()
+                .map(Produto::getValor)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public List<OrdemDeCompra> buscarOrdemDeCompra(){
+        //retorna a lista de ordens de compra da base de dados
+        return new ArrayList<>();
+    }
+
+    public void salvarOrdemCompra(){
+        //salva a ordem de compra
+    }
+
+    public void enviarEmail(){
+        //enviar email da ordem de compra
+    }
+
+    public void imprimirOrdemDeCompra(){
+        //imprimir ordem de compra
+    }
 }
--- src/SRP/problem/Produto.java
+++ src/SRP/problem/Produto.java
@@ -1,5 +1,25 @@
 package SRP.problem;
 
+import java.math.BigDecimal;
+
 public class Produto {
 
+    static BigDecimal valor;
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public Produto( BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Produto() {
+    }
+
+    public void add() { ;
+    }
+
+    public void remove() {
+    }
 }
